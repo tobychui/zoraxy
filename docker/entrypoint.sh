294d504ee6d911514a90ec848f558c1d637445b5
PassiveLemon <lemonl3mn@protonmail.com>
2024-11-12 12:40:08 -0500

    Symlink ZeroTier var to Zoraxy config

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,10 @@ update-ca-certificates
 echo "CA certificates updated"
 
 if [ "$ZEROTIER" = "true" ]; then
+  if [ ! -d "/opt/zoraxy/config/zerotier/" ]; then
+    mkdir -p /opt/zoraxy/config/zerotier/
+  fi
+  ln -s /opt/zoraxy/config/zerotier/ /var/lib/zerotier-one
   zerotier-one -d
   echo "ZeroTier daemon started"
 fi
