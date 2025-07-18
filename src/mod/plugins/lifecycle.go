9c99f6c734ba024e78bf9b6c94ee470c99ee23bd
Anthony Rubick <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2025-07-17 19:17:06 -0700

    feat(plugins): Implement event system w/ POC events
    Implements the partially created event system with 3 events implemented as proof of concepts.
    
    The 3 events are:
    - `blacklistedIpBlocked`: emitted when a request from a blacklisted IP
    - `accessRuleCreated`: emitted when a new access rule is created
    - `blacklistToggled`: emitted when the blacklist is toggled for a given access rule
    
    Why these events? Because these are the ones I forsee myself needing in the next version of the zoraxy_crowdsec_bouncer
    
    Events are dispatched via a global event manager `plugins.EventSystem.Emit`

--- src/mod/plugins/lifecycle.go
+++ src/mod/plugins/lifecycle.go
@@ -151,6 +151,19 @@ func (m *Manager) StartPlugin(pluginID string) error {
 	thisPlugin.process = cmd
 	thisPlugin.Enabled = true
 
+	// Register event subscriptions
+	if thisPlugin.Spec.SubscriptionsEvents != nil {
+		for eventName := range thisPlugin.Spec.SubscriptionsEvents {
+			eventType := zoraxyPlugin.EventName(eventName)
+			err := EventSystem.Subscribe(thisPlugin.Spec.ID, eventType)
+			if err != nil {
+				m.Log("Failed to subscribe plugin "+thisPlugin.Spec.Name+" to event "+eventName, err)
+			} else {
+				m.Log("Subscribed plugin "+thisPlugin.Spec.Name+" to event "+eventName, nil)
+			}
+		}
+	}
+
 	//Create a new static forwarder router for each of the static capture paths
 	thisPlugin.StartAllStaticPathRouters()
 
@@ -287,6 +300,11 @@ func (m *Manager) StopPlugin(pluginID string) error {
 	err = m.Options.APIKeyManager.RevokeAPIKeysForPlugin(thisPlugin.Spec.ID)
 	if err != nil {
 		m.Log("Failed to revoke API keys for plugin "+thisPlugin.Spec.Name, err)
+	}
+	//Unsubscribe from all events
+	err = EventSystem.UnsubscribeAll(thisPlugin.Spec.ID)
+	if err != nil {
+		m.Log("Failed to unsubscribe plugin "+thisPlugin.Spec.Name+" from events", err)
 	}
 
 	return nil
