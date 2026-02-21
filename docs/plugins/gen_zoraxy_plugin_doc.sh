fa3d809f68a92e9e3d46996bec31bfed65644af3
Zen Wen <zen.8841@gmail.com>
2026-02-21 13:22:51 +0800

    Add Shebang for Script

--- docs/plugins/gen_zoraxy_plugin_doc.sh
+++ docs/plugins/gen_zoraxy_plugin_doc.sh
@@ -1,19 +1,18 @@
-#/bin/bash
+#!/bin/bash
 
 # Cd into zoraxy plugin directory
 cd ../../src/mod/plugins/zoraxy_plugin/
 
-
 # Add header to the documentation
-echo "# Zoraxy Plugin APIs" > docs.md
-echo "This API documentation is auto-generated from the Zoraxy plugin source code." >> docs.md
-echo "" >> docs.md
-echo "" >> docs.md
-echo "<pre><code class='language-go'>" >> docs.md
-go doc -all >> docs.md
-echo "</code></pre>" >> docs.md
+echo "# Zoraxy Plugin APIs" >docs.md
+echo "This API documentation is auto-generated from the Zoraxy plugin source code." >>docs.md
+echo "" >>docs.md
+echo "" >>docs.md
+echo "<pre><code class='language-go'>" >>docs.md
+go doc -all >>docs.md
+echo "</code></pre>" >>docs.md
 
-# Replace // import "imuslab.com/zoraxy/mod/plugins/zoraxy_plugin" with 
+# Replace // import "imuslab.com/zoraxy/mod/plugins/zoraxy_plugin" with
 # // import "{{your_module_package_name_in_go.mod}}/mod/plugins/zoraxy_plugin"
 sed -i 's|// import "imuslab.com/zoraxy/mod/plugins/zoraxy_plugin"|// import "{{your_module_package_name_in_go.mod}}/mod/plugins/zoraxy_plugin"|g' docs.md
 
